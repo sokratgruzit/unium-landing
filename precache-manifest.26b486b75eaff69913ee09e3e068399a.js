00e7a41cd1f9e92909b8395f573933632c8afde8
sergo93@gmail.com <sergo93@gmail.com>
2021-01-10 23:09:17 +0400

    gh-pages

---
+++ precache-manifest.26b486b75eaff69913ee09e3e068399a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2749d0acd85e83c60877",
+    "url": "/unium-landing/css/app.be20c59a.css"
+  },
+  {
+    "revision": "cba57db1ea6b2f024edf",
+    "url": "/unium-landing/css/chunk-vendors.b6f3fa05.css"
+  },
+  {
+    "revision": "eec453a5ab3dec41a2923afe6058f29c",
+    "url": "/unium-landing/favicon.png"
+  },
+  {
+    "revision": "37fefefcfd1a1e4dd4b05c2125ed78cf",
+    "url": "/unium-landing/fonts/Ubuntu-Medium.37fefefc.woff"
+  },
+  {
+    "revision": "51496ddf1ce33c063212ff1323c0e7b7",
+    "url": "/unium-landing/fonts/Ubuntu-Medium.51496ddf.woff2"
+  },
+  {
+    "revision": "ac8b5b83c8e51b774af0c27ce33a8c74",
+    "url": "/unium-landing/fonts/Ubuntu-Medium.ac8b5b83.ttf"
+  },
+  {
+    "revision": "c765ca735377a8e0ef8e3ea4604012ed",
+    "url": "/unium-landing/fonts/Ubuntu-Medium.c765ca73.eot"
+  },
+  {
+    "revision": "83e1352fb2ecd2910e7ec1de48ca9a6b",
+    "url": "/unium-landing/fonts/Ubuntu-Regular.83e1352f.eot"
+  },
+  {
+    "revision": "b7350427f5d9fbceabb3325336d69531",
+    "url": "/unium-landing/fonts/Ubuntu-Regular.b7350427.woff2"
+  },
+  {
+    "revision": "ba4e7809d474b7b6936d09346695c3ae",
+    "url": "/unium-landing/fonts/Ubuntu-Regular.ba4e7809.woff"
+  },
+  {
+    "revision": "f60af72207aa4cce7eb6934b7af46cf0",
+    "url": "/unium-landing/fonts/Ubuntu-Regular.f60af722.ttf"
+  },
+  {
+    "revision": "58a44520df39d6af6b5f9f97398caa89",
+    "url": "/unium-landing/img/Ubuntu-Medium.58a44520.svg"
+  },
+  {
+    "revision": "eaa103bdcae269fcaa6b41525ddd86f4",
+    "url": "/unium-landing/img/Ubuntu-Regular.eaa103bd.svg"
+  },
+  {
+    "revision": "d1c7901f85447ee2b3baa105e9ca634c",
+    "url": "/unium-landing/img/aboutBg.d1c7901f.jpg"
+  },
+  {
+    "revision": "9f7f25ce6fcd8842f58901c5eb18f118",
+    "url": "/unium-landing/img/aboutBgLaptop.9f7f25ce.jpg"
+  },
+  {
+    "revision": "bac26854327eb1a445e6cd11df6f525b",
+    "url": "/unium-landing/img/aboutBgMobile.bac26854.jpg"
+  },
+  {
+    "revision": "374410c04f048811320fcd90f8f949d2",
+    "url": "/unium-landing/img/assetsDesk.374410c0.png"
+  },
+  {
+    "revision": "c4d28fbc0bec3a24267f46e1b94681e9",
+    "url": "/unium-landing/img/assetsMob.c4d28fbc.png"
+  },
+  {
+    "revision": "46bdcb5b3671d2aced8d25c0c0919c72",
+    "url": "/unium-landing/img/logoFoot.46bdcb5b.svg"
+  },
+  {
+    "revision": "d21ca18f150fb8ab81d03ef401a47202",
+    "url": "/unium-landing/img/mainBg.d21ca18f.jpg"
+  },
+  {
+    "revision": "75b8bfa3de6a3ac1c1c6bcc575543a99",
+    "url": "/unium-landing/img/mainBgLaptop.75b8bfa3.jpg"
+  },
+  {
+    "revision": "b5e41cba781d7c8ee216ebb07424dd48",
+    "url": "/unium-landing/img/mainBgMobile.b5e41cba.jpg"
+  },
+  {
+    "revision": "9bc8418aee0e00136e96a19516bdaa2e",
+    "url": "/unium-landing/img/mobileImg.9bc8418a.png"
+  },
+  {
+    "revision": "4453276421adf88ac3ece720a0abd413",
+    "url": "/unium-landing/img/wall1.44532764.png"
+  },
+  {
+    "revision": "f45b0306722e2cc381fdc354fa515e51",
+    "url": "/unium-landing/img/wall2.f45b0306.png"
+  },
+  {
+    "revision": "3ab71ed56267023a6d8482639853aef4",
+    "url": "/unium-landing/img/wall3.3ab71ed5.png"
+  },
+  {
+    "revision": "648c9c8f08013bc242434373fa262bdd",
+    "url": "/unium-landing/index.html"
+  },
+  {
+    "revision": "2749d0acd85e83c60877",
+    "url": "/unium-landing/js/app.0bfbf947.js"
+  },
+  {
+    "revision": "cba57db1ea6b2f024edf",
+    "url": "/unium-landing/js/chunk-vendors.4e56b08e.js"
+  },
+  {
+    "revision": "e283552e714ac50f3b586463766fed67",
+    "url": "/unium-landing/manifest.json"
+  },
+  {
+    "revision": "3784188ea53f7845a837ecf806cb8fdb",
+    "url": "/unium-landing/media/video_footer.3784188e.mp4"
+  },
+  {
+    "revision": "38169b7428134122937f7dd959d18d85",
+    "url": "/unium-landing/media/video_top.38169b74.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/unium-landing/robots.txt"
+  }
+]);
